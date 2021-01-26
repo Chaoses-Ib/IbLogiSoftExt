ab6f28826cc8d57cf7c4561182b70ddce47dfb2e
Chaoses Ib <Chaos-es@outlook.com>
2021-01-26 22:48:20 +0800

    Add project files.

---
+++ Injector/dllmain.cpp
@@ -0,0 +1,132 @@
+﻿#include "pch.h"
+#include <sstream>
+#include <optional>
+#include "Boost/di.hpp"
+#include "helper.hpp"
+
+namespace di = boost::di;
+
+struct QString {
+    static inline const wchar_t* (*utf16)(const QString* t);
+};
+
+class LogitechMouseExt {
+    struct args
+    {
+        DWORD* pdword0;
+        unsigned int* arg;
+        const void* pqstring10;
+        const void* family;
+        const void* pqstring20;
+    };
+    static void LuaDispatchEventDetour(int64_t a1, int a2, int event, args* args) {
+        const wchar* event_str = [event]{
+            switch (event) {
+            case 5: return L"G_PRESSED";
+            case 6: return L"G_RELEASED";
+            case 7: return L"M_PRESSED";
+            case 8: return L"M_RELEASED";
+            case 9: return L"@MODE_CHANGED"; //1~3, 4~6 for G-Shift
+            case 10: return L"MOUSE_BUTTON_PRESSED";  //G6~G20, G1~G5 need setting
+            case 11: return L"MOUSE_BUTTON_RELEASED";
+            case 12: return L"GAME_CONTROLLER_BUTTON_PRESSED";
+            case 13: return L"GAME_CONTROLLER_BUTTON_RELEASED";
+            case 14: return L"GAME_CONTROLLER_BUTTON_PRESSED_AND_RELEASED";
+            case 15: return L"AXIS_EVENT";
+            case 16: return L"POV_EVENT";
+            case 18: return L"@PROFILE_ACTIVATED";
+            default: return L"";
+            };
+        }();
+        DebugOutput(wstringstream() << event_str << L"("<<event<<L")" << L", " << *args->arg);
+        LuaDispatchEvent(a1, a2, event, args); //even doesn't call can't stop
+    }
+    static inline decltype(LuaDispatchEventDetour)* LuaDispatchEvent;
+
+    struct struct_a4
+    {
+        byte_t gap0[8];
+        unsigned int* gbutton;
+        const QString* field_10;
+        const QString* pqstring18;
+        QWORD qword20;
+        const QString* pqstring28;
+    };
+    static void EventFunc13_GButtonDetour(int64_t a1, int a2, int a3, struct_a4* a4) {
+        const wchar* event_str = [a3]{
+            switch (a3) {
+            case 10: return L"@CancelGbuttonFunc";
+            case 11: return L"@SetGbuttonFunc";
+            case 14: return L"@MouseButtonPressed"; //G6~G20, G1~G5 need setting
+            case 15: return L"@MouseButtonReleased";
+            case 29: return L"@ChangeCursorSetting";
+            default: return L"";
+            };
+        }();
+        auto tostring = [](const QString* q) {return q ? QString::utf16(q) : L"null"; };
+        if (a3 == 14 || a3 == 15) {
+            DebugOutput(wstringstream() << L"13: "
+                << hex << a1 << L", " << oct
+                << a2 << L", "
+                << event_str << L"|" << a3 << L", "
+                << *a4->gbutton << L", "
+                << tostring(a4->field_10) << L", "
+                << tostring(a4->pqstring18)
+            );
+        }
+        else {
+            DebugOutput(wstringstream() << L"13: "
+                << hex << a1 << L", " << oct
+                << a2 << L", "
+                << event_str << L"|" << a3 << L", "
+                << a4->gbutton << L", "
+                << a4->field_10 << L", "
+                << a4->pqstring18
+            );
+        }
+        
+        EventFunc13_GButton(a1, a2, a3, a4);
+    }
+    static inline decltype(EventFunc13_GButtonDetour)* EventFunc13_GButton;
+public:
+    LogitechMouseExt() {
+        Module LCore = *makeModule::CurrentProcess();
+        LuaDispatchEvent = LCore.base.offset(0x71CC40);
+        //DebugOutput(wstringstream() << LuaDispatchEvent);
+        auto status = IbDetourAttach(&LuaDispatchEvent, LuaDispatchEventDetour);
+        //DebugOutput(wstringstream() << LuaDispatchEvent << L" " << status);
+
+        EventFunc13_GButton = LCore.base.offset(0x80D410);
+        IbDetourAttach(&EventFunc13_GButton, EventFunc13_GButtonDetour);
+
+        Module Qt5Core = *makeModule::Find(L"Qt5Core.dll");
+        QString::utf16 = (decltype(QString::utf16))GetProcAddress(Qt5Core.handle, "?utf16@QString@@QEBAPEBGXZ");
+        DebugOutput(wstringstream() << L"QString::utf16:" << QString::utf16);
+    }
+    ~LogitechMouseExt() {
+        //IbDetourDetach(&LuaDispatchEvent, LuaDispatchEventDetour);
+    }
+};
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    static optional<LogitechMouseExt> ext;
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        DebugOutput(L"Load");
+        ext = di::make_injector().create<LogitechMouseExt>();
+        break;
+    case DLL_THREAD_ATTACH:
+        break;
+    case DLL_THREAD_DETACH:
+        break;
+    case DLL_PROCESS_DETACH:
+        ext.reset();
+        break;
+    }
+    return TRUE;
+}
